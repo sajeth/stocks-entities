83579f1998e4a5c30785092d215852b8c3ba146e
Sajeth <sajethbabu@gmail.com>
2021-03-29 06:32:49 +0530

    initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,6 @@ module stocks.entities {
     requires java.persistence;
     // requires static org.hibernate.orm.core;
     exports com.saji.stocks.entities.batch to stocks.batch, stocks.core;
-    exports com.saji.stocks.entities.models to stocks.core;
+    exports com.saji.stocks.entities.watchlist to stocks.core;
     exports com.saji.stocks.entities.stock to stocks.core;
 }
