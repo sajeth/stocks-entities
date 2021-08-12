fa05f443535503f0ac737766635319c0c6006654
sajeth <sajethbabu@outlook.com>
2021-08-12 22:56:48 +0530

    Added few files

--- src/main/java/com/saji/stocks/entities/watchlist/WatchlistEntity.java
+++ src/main/java/com/saji/stocks/entities/watchlist/WatchlistEntity.java
@@ -2,11 +2,7 @@ package com.saji.stocks.entities.watchlist;
 
 import com.saji.stocks.entities.models.BaseEntity;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -16,34 +12,10 @@ public class WatchlistEntity extends BaseEntity {
     @Basic(optional = false)
     @Column(name = "SYMBOL", nullable = false)
     private String symbol;
-    @Column(name = "PB", nullable = true)
-    private BigDecimal priceToBook;
-    @Column(name = "PE", nullable = true)
-    private BigDecimal priceToEarnings;
-    @Column(name = "CURRENTRATIO", nullable = true)
-    private BigDecimal currentRatio;
-    @Column(name = "DEBTRATIO", nullable = true)
-    private BigDecimal debtRatio;
-    @Column(name = "ROE", nullable = true)
-    private BigDecimal roe;
-    @Column(name = "eps", nullable = true)
-    private BigDecimal eps;
-    @Column(name = "SALESGROWTH", nullable = true)
-    private BigDecimal salesGrowth;
-    @Column(name = "PROFITGROWTH", nullable = true)
-    private BigDecimal profitGrowth;
-
-    public WatchlistEntity(String symbol, BigDecimal priceToBook, BigDecimal priceToEarnings, BigDecimal currentRatio, BigDecimal debtRatio, BigDecimal roe, BigDecimal eps, BigDecimal salesGrowth, BigDecimal profitGrowth) {
-        this.symbol = symbol;
-        this.priceToBook = priceToBook;
-        this.priceToEarnings = priceToEarnings;
-        this.currentRatio = currentRatio;
-        this.debtRatio = debtRatio;
-        this.roe = roe;
-        this.eps = eps;
-        this.salesGrowth = salesGrowth;
-        this.profitGrowth = profitGrowth;
-    }
+    @Column(name = "PRICE", nullable = true)
+    private BigDecimal price;
+    @Column(name = "QUANTITY", nullable = true)
+    private BigDecimal quantity;
 
     public String getSymbol() {
         return symbol;
@@ -52,72 +24,30 @@ public class WatchlistEntity extends BaseEntity {
     public WatchlistEntity() {
     }
 
-    public BigDecimal getPriceToBook() {
-        return priceToBook;
-    }
-
-    public void setPriceToBook(BigDecimal priceToBook) {
-        this.priceToBook = priceToBook;
+    public WatchlistEntity(String symbol, BigDecimal price, BigDecimal quantity) {
+        this.symbol = symbol;
+        this.price = price;
+        this.quantity = quantity;
     }
 
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
 
-    public BigDecimal getPriceToEarnings() {
-        return priceToEarnings;
+    public BigDecimal getPrice() {
+        return price;
     }
 
-    public void setPriceToEarnings(BigDecimal priceToEarnings) {
-        this.priceToEarnings = priceToEarnings;
+    public void setPrice(BigDecimal price) {
+        this.price = price;
     }
 
-    public BigDecimal getCurrentRatio() {
-        return currentRatio;
+    public BigDecimal getQuantity() {
+        return quantity;
     }
 
-    public void setCurrentRatio(BigDecimal currentRatio) {
-        this.currentRatio = currentRatio;
-    }
-
-    public BigDecimal getDebtRatio() {
-        return debtRatio;
-    }
-
-    public void setDebtRatio(BigDecimal debtRatio) {
-        this.debtRatio = debtRatio;
-    }
-
-    public BigDecimal getRoe() {
-        return roe;
-    }
-
-    public void setRoe(BigDecimal roe) {
-        this.roe = roe;
-    }
-
-    public BigDecimal getEps() {
-        return eps;
-    }
-
-    public void setEps(BigDecimal eps) {
-        this.eps = eps;
-    }
-
-    public BigDecimal getSalesGrowth() {
-        return salesGrowth;
-    }
-
-    public void setSalesGrowth(BigDecimal salesGrowth) {
-        this.salesGrowth = salesGrowth;
-    }
-
-    public BigDecimal getProfitGrowth() {
-        return profitGrowth;
-    }
-
-    public void setProfitGrowth(BigDecimal profitGrowth) {
-        this.profitGrowth = profitGrowth;
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
     }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module stocks.entities {
     requires java.persistence;
     exports com.saji.stocks.entities.batch to stocks.batch, stocks.core;
     exports com.saji.stocks.entities.watchlist to stocks.core;
-    exports com.saji.stocks.entities.years to stocks.core;
+    exports com.saji.stocks.entities.stocks to stocks.core;
     exports com.saji.stocks.entities.sector to stocks.core;
+    exports com.saji.stocks.entities.data to stocks.core;
 }
