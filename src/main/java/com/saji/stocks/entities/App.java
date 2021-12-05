fd72b521e9550c5d90e470c1852550059d9e8756
Sajeth <sajethbabu@gmail.com>
2021-12-05 12:57:30 +0530

    Added few files

--- src/main/java/com/saji/stocks/entities/App.java
+++
@@ -1,7 +0,0 @@
-package com.saji.stocks.entities;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("STOCK-ENTITIES");
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,8 @@
  */
 module stocks.entities {
     requires java.persistence;
-    exports com.saji.stocks.entities.batch to stocks.batch, stocks.core;
-    exports com.saji.stocks.entities.watchlist to stocks.core;
-    exports com.saji.stocks.entities.stocks to stocks.core;
-    exports com.saji.stocks.entities.sector to stocks.core;
-    exports com.saji.stocks.entities.data to stocks.core;
+    exports com.saji.entities.transactions to stocks.batch, stocks.core;
+    exports com.saji.entities.data to stocks.core;
+    exports com.saji.entities.configurations to stocks.core;
+    exports com.saji.entities.trading to stocks.core;
 }
